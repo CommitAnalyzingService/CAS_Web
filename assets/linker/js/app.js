5b6a38675e9f3719bb1a91e8d91589b131a6b9eb
Ben Grawi <onionhouseprod@gmail.com>
2013-12-04 01:44:37 -0500

    Fixed accidental change in last commit

--- assets/linker/js/app.js
+++ assets/linker/js/app.js
@@ -62,11 +62,6 @@ app.controller('RepoCtrl', function($scope, $routeParams, socket) {
 	$scope.loaded = false;
 	socket.get('/repository/' + $routeParams.name, function(response) {
 		//console.log(response);
-		response.repo.commits.forEach(function(commit) {
-			commit.forEach(function(value, key) {
-				if(typeof response.repo.commits)
-			});
-		});
 		$scope.$apply(function() {
 			$scope.loaded = true;
 			$scope.repo = response.repo;
